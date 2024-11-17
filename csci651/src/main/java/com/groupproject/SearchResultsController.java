6f928ab550498f6e7002a8266a5807cab1ad3976
TheftJord <voltstar78@gmail.com>
2024-11-16 20:59:03 -0500

    Started Primary Scene and Controller

--- csci651/src/main/java/com/groupproject/SearchResultsController.java
+++ csci651/src/main/java/com/groupproject/SearchResultsController.java
@@ -3,7 +3,7 @@ package com.groupproject;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-public class SecondaryController {
+public class SearchResultsController {
 
     @FXML
     private void switchToPrimary() throws IOException {
